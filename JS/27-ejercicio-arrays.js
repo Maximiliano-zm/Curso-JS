55928934978c41f65f5515f858cf4c4be9f9d05f
unknown <maximiliano.zuritam@gmail.com>
2021-06-04 05:33:24 -0400

    capitulo 27

---
+++ JS/27-ejercicio-arrays.js
@@ -0,0 +1,72 @@
+/*
+
+1. programa que pida 6 numeros por pantalla y los meta en un array
+2. mostrar el array entero (todos sus elementos ) en el cuerpo de la pagina y en la consola
+3. ordenarlo y mostrarlo
+4. invertir su orden y mostrarlo
+5. mostrar cuantos elementos tiene el array
+6. busqueda de un valor introduccio por el usuario , que nos diga si lo encuenta y su indice
+
+*/
+
+function mostrarArray(Elementos , textoCustom = "") {
+  
+    document.write("Contenido del array " +textoCustom+ "");
+    //document.write("<ul>");
+
+    Elementos.forEach((elemento , index) => {
+    
+    document.write("<li>" +elemento+ "<li/>");
+
+    //document.write("<ul/>");
+});
+}
+
+var Obtenerdatos = "";
+var numeros = new Array(6)
+
+// pedir los 6 valores
+Obtenerdatos = prompt("Obtener datos ")
+
+for(var i = 0 ; i < 5; i++){
+
+    numeros[i] = parseInt(prompt ("introduce un numero "));
+
+
+}
+
+//mostar en el cuerpo de la pagina
+
+mostrarArray(numeros);
+
+//mostrar array en la consola
+console.log(numeros);
+
+//ordenar y mostrar
+
+numeros.sort(function(a,b) { return a-b});
+mostrarArray(numeros , 'ordenados');
+
+//invertir y mostrar 
+numeros.reverse();
+mostrarArray(numeros , 'revertidos');
+
+//contar elementos
+
+document.write(numeros.length());
+
+//busqueda
+
+var busqueda = parseInt(prompt("buscar un numero " , 0));
+ 
+var posicion = numeros.findIndex( numero => numero == busqueda);
+
+if (posicion && posicion != 1) {
+    
+    document.write("ENCONTRADO en la posicion : " , posicion);
+
+}else{
+
+     document.write("NO ENCONTRADO ");
+
+}
