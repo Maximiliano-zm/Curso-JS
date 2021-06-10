4decf3863ce4833685be1f79d9d6d98f713d93e9
unknown <maximiliano.zuritam@gmail.com>
2021-06-10 00:04:10 -0400

    clase 28 - audio - css - jquery

--- JS/28-dom.js
+++ JS/28-dom.js
@@ -1,19 +1,25 @@
 // manipular la extructura funcional de una ETIQUETA de html ej : id , class , style , etc
 
-//conseguir elementos con un ID concreto-----------
+//CONSEGUIR ELEMENTOS CON UN ID CONCRETO -----------
 
 // var caja = document.getElementById("micaja"); //forma para llamar a la id
 
 // # = ID
 // . = class
+// sin nada = modificar directamente el div
+
 var caja = document.querySelector("#micaja"); // forma mas facil 
 
-// conseguir elementos con un ID concreto
+//CONSEGUIR ELEMENTOS CON UN ID CONCRETO // EJ SOLO UN DIV 
 
-function cambiacolor(color) {
-    caja.style.background = color;
+        //funcion que requiere ser ejecutada desde la terminal del browser
+        function cambiacolor(color) {
+        caja.style.background = color; 
     
-}
+        }
+        //---------------------------------------
+        
+//modificacion de la etiqueda html pero con js al igual que css
 
 caja.innerHTML = "!texto desde la caja JS!";
 caja.style.background = "red";
@@ -22,17 +28,57 @@ caja.style.color = "green";
 caja.className = "hola hola 2"
 //------------------------------------------
 
-//conseguir elementos por su etiqueta
-var todoslosdivs = document.getElementsByTagName('div'); //para llamar a todos los DIV dentro de un array  /paso 1 
+//SELECCIONAR MUCHOS ELEMENTOS(ETIQUETAS)
 
-/* modificado por 1
-var ContenidoEntexto = todoslosdivs[2]; //selecciona dentro del array que div voy utilizar para modificar /paso 2
+var todoslosdivs = document.getElementsByTagName('div');
 
-//modifico el div a gusto  // paso 3
-ContenidoEntexto.style.background = "BLUE";
-ContenidoEntexto.style.padding = "20px";
-*/
+var seccion = document.querySelector("#miseccion");
+
+var hr = document.createElement("hr");
+
+var valor;
+ 
+for (valor in todoslosdivs){
+
+    if (typeof todoslosdivs[valor].textContent == 'string') {
+
+    var parrafo = document.createElement("p");//etiqueta <p>
+    var texto = document.createTextNode(todoslosdivs[valor].textContent); //obtener valores de texto del array
+    parrafo.append(texto);
+    seccion.append(parrafo);
+
+    }
+}
+
+seccion.prepend(hr);
 
 
-console.log(todoslosdivs);
+//conseguir elementos por su clase css
+
+var divsRojos  = document.getElementsByClassName('rojo');
+var divsamarillos = document.getElementsByClassName('Amarillo');
+
+divsamarillos[0].style.background = ("yellow");
+
+var div;
+
+for(div in divsRojos){
+
+if (divsRojos[div].className == "rojo") {
+
+    divsRojos[div].style.background = "blue";
+}
+}
+console.log(divsRojos);
+
+//Query Selector
+
+var id = document.querySelector("#Encabezado");
+console.log(id);
+
+var claseRojo = document.querySelector("div.rojo")
+console.log(claseRojo);
+
+var etiqueta = document.querySelector("div");
+console.log(etiqueta);
 
---
+++ JS/audio.js
@@ -0,0 +1,20 @@
+
+/*
+
+var BOX =document.querySelector(".BOX") ;
+
+BOX.style.marginLeft = "196.5px";
+BOX.style.top = "383.5px";
+BOX.style.left = "687.5px";
+BOX.style.background = "gray";
+BOX.style.border = "0px solid yellow";
+BOX.style.width = "320px";
+BOX.style.height = "55px";
+BOX.style.borderRadius = "500px";
+//BOX.style.transition="all 1s";
+
+*/
+
+$(".BOX").draggable();
+
+
